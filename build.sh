bdf77e95921721b7ad979af3d3fee4958090d8de
Abhinav Srivastava <abhisri1997@gmail.com>
2016-03-25 03:48:36 +0530

    update: no. of jobs

--- build.sh
+++ build.sh
@@ -6,5 +6,5 @@ export ARCH=arm
 export SUBARCH=arm
 make cyanogenmod_wt88047_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
 make menuconfig
-make -j4 ARCH=arm CROSS_COMPILE=arm-eabi-
+make -j5 ARCH=arm CROSS_COMPILE=arm-eabi-
 dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
