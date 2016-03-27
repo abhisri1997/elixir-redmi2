68ebf8b88703eb2729924d9f2d136b0e221c11ea
Abhinav Srivastava <abhisri1997@gmail.com>
2016-03-27 23:41:03 +0530

    commit: nodoze added from pirej

--- drivers/input/touchscreen/ft5x06_ts.c
+++ drivers/input/touchscreen/ft5x06_ts.c
@@ -1370,7 +1370,8 @@ static int fb_notifier_callback(struct notifier_block *self,
 	if (evdata && evdata->data && event == FB_EVENT_BLANK &&
 			ft5x06_data && ft5x06_data->client) {
 		blank = evdata->data;
-		if (*blank == FB_BLANK_UNBLANK) {
+                /*Edited from pirej latest commit*/
+		if (*blank == FB_BLANK_UNBLANK || *blank == FB_BLANK_VSYNC_SUSPEND) {
 			if (unblanked_once)
 				ft5x06_ts_resume(&ft5x06_data->client->dev);
 		} else if (*blank == FB_BLANK_POWERDOWN) {
