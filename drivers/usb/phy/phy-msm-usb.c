fc0f0619a2fb159b7d1ed49f61eb945049ffdb66
Abhinav Srivastava <abhisri1997@gmail.com>
2016-04-01 04:30:29 +0530

    added : support for ac and usb fast charge

--- drivers/usb/phy/phy-msm-usb.c
+++ drivers/usb/phy/phy-msm-usb.c
@@ -51,6 +51,7 @@
 #include <linux/qpnp/qpnp-adc.h>
 
 #include <linux/msm-bus.h>
+
 #ifdef CONFIG_FORCE_FAST_CHARGE
 #include <linux/fastchg.h>
 #endif
@@ -1967,13 +1968,13 @@ static void msm_otg_notify_charger(struct msm_otg *motg, unsigned mA)
 		return;
 
 #ifdef CONFIG_FORCE_FAST_CHARGE
- 	if (force_fast_charge > 0 && mA > 0) {
-        	mA = IDEV_ACA_CHG_MAX;
- 		pr_info("USB fast charging is ON\n");
- 	} else {
- 		pr_info("USB fast charging is OFF\n");
- 	}
- #endif
+	if (force_fast_charge > 0 && mA > 0) {
+		mA = IDEV_ACA_CHG_MAX;
+		pr_info("USB fast charging is ON\n");
+	} else {
+		pr_info("USB fast charging is OFF\n");
+	}
+#endif
 
 	dev_info(motg->phy.dev, "Avail curr from USB = %u\n", mA);
 	msm_otg_dbg_log_event(&motg->phy, "AVAIL CURR FROM USB",
@@ -3292,8 +3293,18 @@ static void msm_otg_sm_work(struct work_struct *w)
 				case USB_DCP_CHARGER:
 					/* fall through */
 				case USB_PROPRIETARY_CHARGER:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+					if (force_fast_charge > 0) {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX_FAST);						
+					} else {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX);
+					}
+#else
 					msm_otg_notify_charger(motg,
 							IDEV_CHG_MAX);
+#endif
 					otg->phy->state =
 						OTG_STATE_B_CHARGER;
 					work = 0;
@@ -3304,8 +3315,18 @@ static void msm_otg_sm_work(struct work_struct *w)
 					pm_runtime_put_sync(otg->phy->dev);
 					break;
 				case USB_FLOATED_CHARGER:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+					if (force_fast_charge > 0) {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX_FAST);						
+					} else {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX);
+					}
+#else
 					msm_otg_notify_charger(motg,
 							IDEV_CHG_MAX);
+#endif
 					otg->phy->state =
 						OTG_STATE_B_CHARGER;
 					work = 0;
@@ -3325,8 +3346,18 @@ static void msm_otg_sm_work(struct work_struct *w)
 					 */
 					break;
 				case USB_CDP_CHARGER:
+#ifdef CONFIG_FORCE_FAST_CHARGE
+					if (force_fast_charge > 0) {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX_FAST);						
+					} else {
+						msm_otg_notify_charger(motg,
+								IDEV_CHG_MAX);
+					}
+#else
 					msm_otg_notify_charger(motg,
 							IDEV_CHG_MAX);
+#endif
 					msm_otg_start_peripheral(otg, 1);
 					otg->phy->state =
 						OTG_STATE_B_PERIPHERAL;
