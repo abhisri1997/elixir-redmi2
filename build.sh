f1c510664b6e74662ed40443d1025641bb29622c
Abhinav Srivastava <abhisri1997@gmail.com>
2016-04-01 05:56:15 +0530

    updates: defconfig

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ export USE_CCACHE=1
 export CCACHE_DIR=~/android/cache
 export ARCH=arm
 export SUBARCH=arm
-make cyanogenmod_wt88047_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
+make elixir_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
 make menuconfig
 make -j5 ARCH=arm CROSS_COMPILE=arm-eabi-
