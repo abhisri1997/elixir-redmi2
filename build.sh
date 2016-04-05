7d0b663b10cc91e4b7f1e890303b39a58a33b010
Abhinav Srivastava <abhisri1997@gmail.com>
2016-04-05 11:58:57 +0530

    reverted gpu oc/uc to normal

--- build.sh
+++ build.sh
@@ -7,3 +7,4 @@ export SUBARCH=arm
 make elixir_defconfig ARCH=arm CROSS_COMPILE=arm-eabi-
 make menuconfig
 make -j5 ARCH=arm CROSS_COMPILE=arm-eabi-
+dtbToolCM -2 -o $KERNEL_DIR/arch/arm/boot/dt.img -s 2048 -p $KERNEL_DIR/scripts/dtc/ $KERNEL_DIR/arch/arm/boot/dts/
